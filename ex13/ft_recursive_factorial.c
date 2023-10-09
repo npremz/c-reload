862ecd103a60636fad9c9e1a098367dedfaef7e4
Nicolas Premont <npremont@shi-r1-p2.s19.be>
2023-10-09 18:06:16 +0200

    corr

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,23 +6,24 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:05:01 by npremont          #+#    #+#             */
-/*   Updated: 2023/10/09 15:50:13 by npremont         ###   ########.fr       */
+/*   Updated: 2023/10/09 18:05:28 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
-		return (0);
-	if (nb == 1 || nb == 0)
+	if (nb > 1 && nb <=12)
+		return (ft_recursive_factorial(nb - 1) * nb);
+	else if (nb == 1 || nb == 0)
 		return (1);
-	return (nb * ft_recursive_factorial(nb -1));
+	else
+		return (0);
 }
 
 // #include <stdio.h>
 
 // int	main(void)
 // {
-// 	printf("%d\n", ft_recursive_factorial(5));
+// 	printf("%d\n", ft_recursive_factorial(14));
 // 	return (0);
 // }
